6417ffa0778f442886134d6829df7c4a5fa220f0
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2025-08-05 22:50:42 +0530

    Updated App.js for bloodPressure route using AutoPilot

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const bloodPressureRoute = require('./routes/bloodPressure');
+app.use('/bloodPressure', bloodPressureRoute);
 const usersRoute = require('./routes/users');
 app.use('/users', usersRoute);
 
