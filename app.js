ef851443c05b8154e64a057b29bc0f7c10980af7
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2025-08-05 22:48:09 +0530

    Updated App.js for users route using AutoPilot

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const usersRoute = require('./routes/users');
+app.use('/users', usersRoute);
 
 const conn = require('./middlewares/connection');
 console.log("Trying to establish a connection to TiDB...");
